38464ce2a20b5854fdaf836c2d64d320c73e7021
tachibanatowa <tachibana.towa@flaggs.co.jp>
2023-03-17 11:58:05 +0900

    ファーストコミット

---
+++ Assets/CharacteMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class CharacteMove
+{
+    //キャラクターの移動速度
+    public float speed;
+    //キャラクターの座標取得用
+    public float posx;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        speed = 0.05f;
+        posx = 0.0f;
+    }
+
+    // Update is called once per frame
+    
+    public void MoveA(float posx)
+    {
+        posx += speed;
+    }
+
+    public void MoveD(float posx)
+    {
+        posx -= speed;
+    }
+
+    public float MoveReturn()
+    {
+        return posx;
+    }
+
+    public void Update()
+    {
+
+    }
+}
---
+++ Assets/MainGameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameController : MonoBehaviour
+{
+    public CharacteMove Charmove;
+
+    public float xpos=0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Charmove = new CharacteMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キャラクターの座標の座標を取得する
+        Vector2 position = transform.position;
+        
+        if (Input.GetKey (KeyCode.A))
+        {
+            xpos = position.x;
+            Charmove.MoveA(xpos);
+            position.x = Charmove.MoveReturn();
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            xpos = position.x;
+            Charmove.MoveD(xpos);
+            position.x = Charmove.MoveReturn();
+        }
+        transform.position = position;
+
+    }
+}
