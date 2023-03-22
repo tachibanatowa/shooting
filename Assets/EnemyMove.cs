57e18cf2d1ce4899e58c38b79e8c065d86d0caed
tachibanatowa <tachibana.towa@flaggs.co.jp>
2023-03-22 10:56:28 +0900

    敵クラス、プレハブを作成（移動、プレハブの自動生成）を対応

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.01f, 0);
+
+        if (transform.position.y < -6.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/EnemySpawnControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnControl : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	private float interval = 1.0f;
+	private float timeElapsed = 0.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed >= interval)
+		{
+			timeElapsed = 0.0f;
+			Vector3 randamPos = new Vector3(Random.Range(3.7f, -3.7f), 6.0f, 0);
+			Instantiate(enemyPrefab, randamPos, Quaternion.identity);
+		}
+	}
+}
